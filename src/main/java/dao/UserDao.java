5ef210bc294d4981bb4931acbb6e9dba24a727e0
igor.lazarev <lazermann111@gmail.com>
2021-10-15 21:09:30 +0300

    Added User db - WIP

--- src/main/java/dao/UserDao.java
+++ src/main/java/dao/UserDao.java
@@ -1,7 +1,23 @@
 package dao;
 
+import model.User;
+
+import java.util.List;
+
 /**
  * Created by Igor on 10/8/2019.
  */
 public interface UserDao {
+
+    void save(User user);
+
+    void update(User user);
+
+    void delete(User user);
+
+    User getByName(String name);
+
+    List<User> getAll();
+
+    User getById(long id);
 }
---
+++ src/main/java/dao/UserDaoImpl.java
@@ -0,0 +1,39 @@
+package dao;
+
+import model.User;
+
+import java.util.List;
+
+public class UserDaoImpl implements UserDao{
+
+
+    @Override
+    public void save(User user) {
+
+    }
+
+    @Override
+    public void update(User user) {
+
+    }
+
+    @Override
+    public void delete(User user) {
+
+    }
+
+    @Override
+    public User getByName(String name) {
+        return null;
+    }
+
+    @Override
+    public List<User> getAll() {
+        return null;
+    }
+
+    @Override
+    public User getById(long id) {
+        return null;
+    }
+}
